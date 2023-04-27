09b2902c50f9f1bc52fe98847fa9106420e5eca6
Sherry-Ma <shaorong.ma@duke.edu>
2023-04-27 00:58:02 +0000

    update loadtest

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,21 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, between, task
+import csv
+import random
 
+# Define a user class to simulate requests
 class MyUser(HttpUser):
-    wait_time = between(5, 9)
+    # Set the wait time between requests
+    wait_time = between(5, 15)
+    
+    # Load the messages from the CSV file into a list
+    with open('ham_spam_test.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        messages = [row['Text'] for row in reader]
 
+    # Define a task that sends a POST request to the API endpoint with a message from the CSV file
     @task
-    def predict(self):
-        payload = {'message': 'Hello world'}
-        self.client.post('/predict', json=payload)
+    def send_message(self):
+        # Choose a random message from the list
+        message = random.choice(self.messages)
+        # Send a POST request to the API endpoint with the selected message
+        self.client.post('/predict', json={'message': message})
